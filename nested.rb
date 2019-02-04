41f13fd65a74e61413bf75ba0dfcdba53857a46e
Igor Rud <rud.igor23@gmail.com>
2019-02-04 22:06:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
         }
      }
 		 programmer_hash[:yukihiro_matsumoto] = Hash.new
-		 programmer_hash[:yukihiro_matsumoto][] = :known_for, :languages
+		 programmer_hash[:yukihiro_matsumoto] = :known_for, :languages
 end
 
 def changing_alan
