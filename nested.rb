c4b70ad3d41cdf323b59380ac8a4887f0f7b2d93
Igor Rud <rud.igor23@gmail.com>
2019-02-04 22:17:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 programmer_hash[:yukihiro_matsumoto] = Hash.new
+		 programmer_hash[:yukihiro_matsumoto] = Hash.new {:known_for => "Ruby", :languages => ["LISP", "C"]}
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
 
